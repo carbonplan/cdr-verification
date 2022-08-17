4bc98debcdd71f45c88e2ca1e24687c4a31b04bb
katamartin <katasm@gmail.com>
2022-08-17 16:43:48 -0700

    Begin to add uncertainty information

--- components/element.js
+++ components/element.js
@@ -1,15 +1,28 @@
-import { Box, Flex } from 'theme-ui'
-import { Expander } from '@carbonplan/components'
+import { Box, Divider, Flex } from 'theme-ui'
+import { Badge, Expander } from '@carbonplan/components'
 import Circle from './circle'
+import { CATEGORY_MAPPING } from './constants'
 
 const Element = ({
   category,
   comments,
   diagram_component,
   description,
+  uncertainty_type,
+  uncertainty_magnitude,
+  responsibility,
   active,
   onClick,
 }) => {
+  const sx = {
+    heading: {
+      color: CATEGORY_MAPPING[category],
+      letterSpacing: 'smallcaps',
+      textTransform: 'uppercase',
+    },
+    badge: { textTransform: 'capitalize' },
+  }
+
   return (
     <Box sx={{ my: [2, 3, 3, 4] }}>
       <Flex sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
@@ -28,7 +41,20 @@ const Element = ({
         </Flex>
         <Expander value={active} onClick={onClick} sx={{ flexShrink: 0 }} />
       </Flex>
-      {active ? comments : null}
+
+      {active ? (
+        <Box>
+          <Divider />
+          <Box sx={sx.heading}>Uncertainty type</Box>
+          <Badge sx={sx.badge}>{uncertainty_type}</Badge>
+          <Box sx={sx.heading}>Uncertainty responsibility</Box>
+          <Badge sx={sx.badge}>{responsibility}</Badge>
+          <Box sx={sx.heading}>Notes</Box>
+
+          <Box sx={{ fontFamily: 'faux' }}>{comments}</Box>
+          <Divider />
+        </Box>
+      ) : null}
     </Box>
   )
 }
