d33f53702c2bdcc0f080264a231a0860a6d2773f
katamartin <katasm@gmail.com>
2022-09-02 08:51:28 -0700

    Use grid to align selector and VCL badge

--- components/pathway-info.js
+++ components/pathway-info.js
@@ -15,33 +15,31 @@ const PathwayInfo = ({ pathway, setPathway, size = 'sm' }) => {
   const formattedVCL = VCL[0] === VCL[1] ? VCL[0] : VCL.join('-')
   return (
     <Row columns={[6, 6, 7, 7]}>
+      <Column start={1} width={[4, 4, 5, 5]} sx={{ mt: [0, 0, 0, '15px'] }}>
+        <PathwaySelector
+          size={size}
+          pathway={pathway}
+          setPathway={setPathway}
+        />
+      </Column>
+      <Column start={[5, 5, 6, 6]} width={2} sx={{ mt: [0, 0, 0, '15px'] }}>
+        <Badge
+          sx={{
+            mt: ['0px', '0px', '0px', '1px'],
+            mb: ['2px', '2px', '2px', '1px'],
+            fontSize: 3,
+            height: null,
+            padding: 1,
+            flexShrink: 0,
+          }}
+        >
+          <Box as='span' sx={{ color: 'secondary' }}>
+            VCL
+          </Box>{' '}
+          {formattedVCL}
+        </Badge>
+      </Column>
       <Column start={1} width={[6, 6, 6, 6]}>
-        <Flex sx={{ gap: [3, 3, 3, 4], mt: [0, 0, 0, '15px'] }}>
-          <PathwaySelector
-            size={size}
-            pathway={pathway}
-            setPathway={setPathway}
-          />
-          <Badge
-            sx={{
-              mt: ['0px', '0px', '0px', '1px'],
-              mb: ['2px', '2px', '2px', '1px'],
-              mr:
-                formattedVCL.length === 1
-                  ? ['21px', '21px', '21px', '27px']
-                  : 0,
-              fontSize: 3,
-              height: null,
-              padding: 1,
-              flexShrink: 0,
-            }}
-          >
-            <Box as='span' sx={{ color: 'secondary' }}>
-              VCL
-            </Box>{' '}
-            {formattedVCL}
-          </Badge>
-        </Flex>
         <Box sx={{ my: [3, 3, 3, 4], fontSize: [1] }}>
           {pathway_description}
         </Box>
