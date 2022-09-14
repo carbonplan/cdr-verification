d1a7bccdd8ec17e0ed6a90c394a560197abfad11
katamartin <katasm@gmail.com>
2022-09-14 12:42:47 -0700

    Render circle icons

--- components/circle.js
+++ components/circle.js
@@ -1,50 +1,39 @@
-import { Box, Flex } from 'theme-ui'
+import { Box, useThemeUI } from 'theme-ui'
 import { mix } from '@theme-ui/color'
 
 import { CATEGORY_COLORS } from './constants'
 import { useElement } from './context/element'
+import CIRCLE_ICONS from './circle-icons'
 
 const Circle = ({ id, sx, ...props }) => {
+  const { theme } = useThemeUI()
   const { status, data } = useElement(id)
 
   let mixer
   switch (status) {
     case 'hovered':
-      mixer = (color) => 'primary'
+      mixer = (color) => theme.colors.primary
       break
     case 'inactive':
-      mixer = (color) => mix(color, 'background', 0.5)
+      mixer = (color) => mix(color, 'background', 0.5)(theme)
       break
     default:
-      mixer = (color) => color
+      mixer = (color) => theme.colors[color]
       break
   }
 
   const color = CATEGORY_COLORS[data.category] ?? 'secondary'
+  const Icon = CIRCLE_ICONS[id]
+
+  if (!Icon) {
+    console.warn(`No icon found for id: ${id}`)
+    return null
+  }
+
   return (
-    <Flex
-      sx={{
-        backgroundColor: 'background',
-        borderWidth: '1px',
-        borderStyle: id.includes('*') ? 'dashed' : 'solid',
-        borderColor: mixer ? mixer(color) : color,
-        alignContent: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        borderRadius: '24px',
-        width: '24px',
-        height: '24px',
-        color: mixer ? mixer(color) : color,
-        fontSize: 1,
-        transition: '0.15s',
-        fontFamily: 'body',
-        letterSpacing: 'body',
-        ...sx,
-      }}
-      {...props}
-    >
-      <Box sx={{ mt: '1px' }}>{id.replace('*', '')}</Box>
-    </Flex>
+    <Box sx={sx}>
+      <Icon color={mixer(color)} background={theme.colors.background} />
+    </Box>
   )
 }
 
