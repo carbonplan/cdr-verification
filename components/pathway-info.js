f537fdf2e2cd44577009287c55ddc7e1262fb1c5
katamartin <katasm@gmail.com>
2022-09-15 15:39:03 -0700

    Tweak desktop horizontal alignment

--- components/pathway-info.js
+++ components/pathway-info.js
@@ -1,4 +1,4 @@
-import { Box } from 'theme-ui'
+import { Box, Flex } from 'theme-ui'
 import { Badge, Row, Column } from '@carbonplan/components'
 
 import PathwaySelector from './pathway-selector'
@@ -19,38 +19,35 @@ const PathwayInfo = ({ pathway, setPathway }) => {
   return (
     <Row columns={[6, 6, 7, 7]}>
       <Column start={1} width={[4, 4, 5, 4]} sx={{ mt: [0, 0, 0, '15px'] }}>
-        <PathwaySelector
-          size={33.3}
-          pathway={pathway}
-          setPathway={setPathway}
-        />
-      </Column>
-      <Column
-        start={[5, 5, 6, 5]}
-        width={[2, 2, 2, 2]}
-        sx={{ mt: [0, 0, 0, '15px'] }}
-      >
         <TooltipWrapper
           expanded={expanded}
           setExpanded={setExpanded}
           align='center'
           mt='10px'
         >
-          <Badge
-            sx={{
-              mt: ['0px', '0px', '0px', '1px'],
-              mb: ['2px', '2px', '2px', '1px'],
-              fontSize: 3,
-              height: null,
-              padding: 1,
-              flexShrink: 0,
-            }}
-          >
-            <Box as='span' sx={{ color: 'secondary' }}>
-              VCL
-            </Box>{' '}
-            {formattedVCL}
-          </Badge>
+          <Flex sx={{ gap: 3 }}>
+            <PathwaySelector
+              size={33.3}
+              pathway={pathway}
+              setPathway={setPathway}
+            />
+            <Badge
+              sx={{
+                mt: ['0px', '0px', '0px', '1px'],
+                mb: ['2px', '2px', '2px', '1px'],
+                fontSize: 3,
+                height: null,
+                padding: 1,
+                flexShrink: 0,
+                height: 'fit-content',
+              }}
+            >
+              <Box as='span' sx={{ color: 'secondary' }}>
+                VCL
+              </Box>{' '}
+              {formattedVCL}
+            </Badge>
+          </Flex>
         </TooltipWrapper>
       </Column>
       <Column start={1} width={[6, 6, 6, 5]}>
