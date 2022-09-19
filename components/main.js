e800e3d4473b2d25eb32a5cdce2fabf9800088c8
katamartin <katasm@gmail.com>
2022-09-18 18:04:01 -0700

    Tweak routing

--- components/main.js
+++ components/main.js
@@ -40,7 +40,7 @@ const Main = ({ pathway }) => {
   const closeTray = useCallback(() => setSettings(false), [])
 
   const setPathway = useCallback((name) => {
-    router.replace(`/research/cdr-verification/${name}`)
+    router.replace(`${name}`)
   })
 
   useEffect(() => {
