0b3da7c258537b57f5ec084d565285300474403d
katamartin <katasm@gmail.com>
2022-09-18 17:31:36 -0700

    Add asset prefix for production to next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const isDev =
+  process.env.VERCEL_ENV === 'preview' || process.env.NODE_ENV === 'development'
+
 module.exports = {
   pageExtensions: ['jsx', 'js'],
+  assetPrefix: isDev ? '' : 'https://cdr-verification.carbonplan.org',
 }
