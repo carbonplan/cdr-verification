23403cf49178615bb6fab1bf43222737d7f9e45c
katamartin <katasm@gmail.com>
2023-03-31 16:46:24 -0700

    Fix EW/1.0

--- components/flow-diagrams/EW/1.0.js
+++ components/flow-diagrams/EW/1.0.js
@@ -6,7 +6,7 @@ const Flow = () => {
   return (
     <Grid height={29}>
       <Rectangle
-        id='8'
+        id='mat_co2'
         start={[1, 2]}
         height={1}
         width={4}
@@ -14,7 +14,7 @@ const Flow = () => {
       />
 
       <Rectangle
-        id='9'
+        id='energy_co2'
         start={[4, 2]}
         height={1}
         width={4}
@@ -31,28 +31,28 @@ const Flow = () => {
         secondary
       />
 
-      <Rectangle start={[3, 7]} id='1' width={4} />
+      <Rectangle start={[3, 7]} id='rock_pot' width={4} />
 
       <Arrow start={[8, 9]} length={2} right />
 
-      <Rectangle width={4} id='2' start={[11, 7]} />
+      <Rectangle width={4} id='nwp' start={[11, 7]} />
 
       <Arrow start={[16, 9]} length={1} right hideArrow />
       <Arrow start={[17, 6]} length={6} up hideArrow />
       <Arrow start={[17, 6]} length={2} right />
       <Arrow start={[17, 12]} length={2} right />
 
-      <Rectangle id='5' start={[20, 5]} />
+      <Rectangle id='carb_surf' start={[20, 5]} />
 
       <Rectangle label='Alkalinity run-off' start={[20, 11]} height={3} />
 
       <Arrow start={[27, 6]} length={2} left />
 
-      <Rectangle id='13' start={[30, 5]} />
+      <Rectangle id='carb_surf_durability' start={[30, 5]} />
 
       <Arrow start={[23, 15]} length={2} down />
       <Rectangle
-        id='6'
+        id='evasion'
         start={[21, 19]}
         width={4}
         height={1}
@@ -62,22 +62,22 @@ const Flow = () => {
       <Arrow start={[27, 12]} length={2} right />
       <Rectangle height={3} start={[30, 11]} label='Ocean storage' />
       <Arrow start={[33, 15]} length={2} up />
-      <Rectangle id='14' height={3} start={[30, 18]} />
+      <Rectangle id='tau_dic' height={3} start={[30, 18]} />
 
       <Arrow start={[5, 12]} length={2} down />
 
-      <Rectangle start={[2, 15]} width={6} height={3} id='7*' />
+      <Rectangle start={[2, 15]} width={6} height={3} id='soil_c' />
 
-      <Rectangle start={[2, 19]} width={6} height={3} id='10*' />
+      <Rectangle start={[2, 19]} width={6} height={3} id='lime_reduce' />
 
-      <Rectangle start={[2, 23]} width={6} height={3} id='11*' />
+      <Rectangle start={[2, 23]} width={6} height={3} id='fert_reduce' />
 
-      <Rectangle start={[2, 27]} width={6} height={3} id='12*' />
+      <Rectangle start={[2, 27]} width={6} height={3} id='n2o_reduce' />
 
       <Arrow start={[13, 12]} length={2} down />
 
-      <Rectangle id='3' height={3} start={[10, 15]} />
-      <Rectangle id='4' height={3} start={[10, 19]} />
+      <Rectangle id='alk_loss' height={3} start={[10, 15]} />
+      <Rectangle id='min_sec' height={3} start={[10, 19]} />
     </Grid>
   )
 }
