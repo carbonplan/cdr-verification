e00991e00b715ff5261aa20b00b2f641133dd23c
katamartin <katasm@gmail.com>
2023-04-03 09:51:39 -0700

    Add existing methods doc

--- pages/_app.js
+++ pages/_app.js
@@ -10,34 +10,39 @@ const App = ({ Component, pageProps }) => {
   const [settings, setSettings] = useState(false)
   const router = useRouter()
   const isDocs = router.pathname.includes('/docs/')
+  const isMethods = router.pathname.includes('cdr-verification-methods')
 
   return (
     <ThemeProvider theme={theme}>
-      <Layout
-        title='CDR Verification Framework – CarbonPlan'
-        description='An interactive tool for understanding CDR verification
+      {isMethods ? (
+        <Component {...pageProps} />
+      ) : (
+        <Layout
+          title='CDR Verification Framework – CarbonPlan'
+          description='An interactive tool for understanding CDR verification
       by exploring key uncertainties around carbon removal and
       durability outcomes for different CDR pathways'
-        card='https://images.carbonplan.org/social/cdr-verification.png'
-        container={false}
-        footer={false}
-        metadata={false}
-        nav={'research'}
-        settings={
-          isDocs
-            ? null
-            : {
-                value: settings,
-                onClick: () => setSettings((prev) => !prev),
-              }
-        }
-      >
-        <Component
-          {...pageProps}
-          settings={settings}
-          setSettings={setSettings}
-        />
-      </Layout>
+          card='https://images.carbonplan.org/social/cdr-verification.png'
+          container={false}
+          footer={false}
+          metadata={false}
+          nav={'research'}
+          settings={
+            isDocs
+              ? null
+              : {
+                  value: settings,
+                  onClick: () => setSettings((prev) => !prev),
+                }
+          }
+        >
+          <Component
+            {...pageProps}
+            settings={settings}
+            setSettings={setSettings}
+          />
+        </Layout>
+      )}
     </ThemeProvider>
   )
 }
