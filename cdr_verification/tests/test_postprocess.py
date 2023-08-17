424746917a1d2a00d8805cf364aa5019371d910a
Raphael Hagen <norlandrhagen@gmail.com>
2023-08-16 17:04:50 -0700

    test_pathways_version_note_bool

--- cdr_verification/tests/test_postprocess.py
+++ cdr_verification/tests/test_postprocess.py
@@ -1,6 +1,6 @@
 import pytest
 from common import send_slack_notification, test_google_doc_id, test_gsheet_doc_name, avail_pathways
-from postprocess import generate_combined_pathway_data_dict, equation_number_component_number
+from postprocess import generate_combined_pathway_data_dict, equation_number_component_number, pathways_version_note_bool
 
 # The goal of these tests are to pull data from the test workbook
 # Which has examples of each error and test each postprocess step
@@ -15,15 +15,20 @@ from postprocess import generate_combined_pathway_data_dict, equation_number_com
 # - [ x ]  (Static - Postprocess) There is a version note corresponding with the current version number
 # - [ x ]  (Static - Postprocess) All equation numbers correspond with numbered components (ie all numbers in equation are a subset of the numbers found in the number column)
 
-metadata_combined = generate_combined_pathway_data_dict(test_google_doc_id, avail_pathways)
+notification_flag = False 
 
-def test_equation_number_component_number(metadata_combined: dict):
-    df = equation_number_component_number(metadata_combined=metadata_combined)
-    # Assert part or whole dataframe matches expected failure mode
+ 
+def test_equation_number_component_number():
+    metadata_combined = generate_combined_pathway_data_dict(test_google_doc_id, avail_pathways)
+    df = equation_number_component_number(metadata_combined=metadata_combined, notification=notification_flag)
+    assert df['equation_number_bool'].all() == False
+
+def test_pathways_version_note_bool():
+    metadata_combined = generate_combined_pathway_data_dict(test_google_doc_id, avail_pathways)
+    df = pathways_version_note_bool(metadata_combined=metadata_combined, notification=notification_flag)
+    assert df['latest_version_note_exists'].all() == False
 
 
-
-# equation_number_component_number(metadata_dict=metadata_combined)
 # pathways_version_note_bool(metadata_dict=metadata_combined)
 # pathway_componets_sheets_subset(metadata_dict=metadata_combined)
 # pathway_id_sheets_subset(metadata_dict=metadata_combined)
