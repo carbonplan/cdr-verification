5923514ea8a76735f1989622c843afceeb363756
Raphael Hagen <norlandrhagen@gmail.com>
2023-08-22 16:18:13 -0700

    additional pathways level test

--- cdr_verification/common.py
+++ cdr_verification/common.py
@@ -1,46 +1,83 @@
+import os
 
-
-from googleapiclient.discovery import build
+import pandas as pd
+import requests
 from google.oauth2.service_account import Credentials
-import os 
-import requests 
-import pandas as pd 
-
+from googleapiclient.discovery import build
 
 # ---------------------------------------------------------------------------------------------
 #                                 IDs and Names
 # ---------------------------------------------------------------------------------------------
 
 
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 
 # Production Ids/Names
 google_doc_id = '1wYGtKe6ex27wei-eo6oh7hgLHrMXn9wmpqGS_l05lCw'
 gsheet_doc_name = 'NEW_CDR MRV Pathway Uncertainties'
 
-avail_pathways = ['DAC', 'BiCRS','EW','TER_BIO','OCEAN_BIO_no_harvest','OCEAN_BIO_harvest','OAE_echem','OAE_mineral', 'DOR','BIOCHAR','ALK_WASTE_MIN']
-pathways_data_columns = ['number','category','component_id','name','quantification_target','uncertainty_type','responsibility','uncertainty_impact_min','uncertainty_impact_max','description','notes']
-components_non_pathway_cols = ['revisions','notes','category','component_id','component_name','secondary_name','quantification_target','uncertainty_type','responsibility','uncertainty_impact_min','uncertainty_impact_max','description']
+avail_pathways = [
+    'DAC',
+    'BiCRS',
+    'EW',
+    'TER_BIO',
+    'OCEAN_BIO_no_harvest',
+    'OCEAN_BIO_harvest',
+    'OAE_echem',
+    'OAE_mineral',
+    'DOR',
+    'BIOCHAR',
+    'ALK_WASTE_MIN',
+]
+pathways_data_columns = [
+    'number',
+    'category',
+    'component_id',
+    'name',
+    'quantification_target',
+    'uncertainty_type',
+    'responsibility',
+    'uncertainty_impact_min',
+    'uncertainty_impact_max',
+    'description',
+    'notes',
+]
+components_non_pathway_cols = [
+    'revisions',
+    'notes',
+    'category',
+    'component_id',
+    'component_name',
+    'secondary_name',
+    'quantification_target',
+    'uncertainty_type',
+    'responsibility',
+    'uncertainty_impact_min',
+    'uncertainty_impact_max',
+    'description',
+]
 
-uncertainty_values = ['not characterized','negligible', 'low', 'medium', 'high', 'very high']
-responsibility_values = ['system','project']
+uncertainty_values = ['not characterized', 'negligible', 'low', 'medium', 'high', 'very high']
+responsibility_values = ['system', 'project']
 
-sheet_id_dict = {'components': '552961890',
-'DAC': '1836739710', 
-'BiCRS': '315597137', 
-'EW': '83914401', 
-'TER_BIO': '1216202441', 
-'OCEAN_BIO_no_harvest': '141361092', 
-'OCEAN_BIO_harvest': '634124525', 
-'OAE_echem': '960206408', 
-'OAE_mineral': '670121219', 
-'DOR': '2050001783', 
-'BIOCHAR': '1891518923', 
-'ALK_WASTE_MIN': '2039248866',
-'CONTRIBUTORS': '959291730',
-'contributor_test': '1652596480',
-'pathways_test':'1764033321',
-'components_test': '1032325311'}
+sheet_id_dict = {
+    'components': '552961890',
+    'DAC': '1836739710',
+    'BiCRS': '315597137',
+    'EW': '83914401',
+    'TER_BIO': '1216202441',
+    'OCEAN_BIO_no_harvest': '141361092',
+    'OCEAN_BIO_harvest': '634124525',
+    'OAE_echem': '960206408',
+    'OAE_mineral': '670121219',
+    'DOR': '2050001783',
+    'BIOCHAR': '1891518923',
+    'ALK_WASTE_MIN': '2039248866',
+    'CONTRIBUTORS': '959291730',
+    'contributor_test': '1652596480',
+    'pathways_test': '1764033321',
+    'components_test': '1032325311',
+}
 
 # Test Ids and Names
 
@@ -56,29 +93,32 @@ def auth_service():
     try:
         cred_path = os.path.expanduser('~/keybase/google-sheets-key.json')
 
-        creds = Credentials.from_service_account_file(
-        cred_path,    scopes=scope
-        )   
+        creds = Credentials.from_service_account_file(cred_path, scopes=scope)
         service = build('sheets', 'v4', credentials=creds)
     except:
         service = build('sheets', 'v4')
-    return service 
+    return service
+
 
 def send_slack_notification(df: pd.DataFrame, validation_name: str):
     """Send slack notifications to the cdr-verification-updates slack channel
 
-    :param df: Input DataFrame of reported inconsistencies 
+    :param df: Input DataFrame of reported inconsistencies
     :type df: pd.DataFrame
     :param validation_name: A description of the validation check
     :type validation_name: str
     """
     dataframe_markdown = df.to_markdown()
-    slack_webhook_url = os.environ.get("SLACK_WEBHOOK_URL")
-    validation_description = pd.DataFrame({'Validation': [validation_name]}).to_markdown(index=False)
-    r = requests.post(slack_webhook_url, json={'text': f'```{validation_description}``` ```{dataframe_markdown}```'})
+    slack_webhook_url = os.environ.get('SLACK_WEBHOOK_URL')
+    validation_description = pd.DataFrame({'Validation': [validation_name]}).to_markdown(
+        index=False
+    )
+    r = requests.post(
+        slack_webhook_url,
+        json={'text': f'```{validation_description}``` ```{dataframe_markdown}```'},
+    )
 
     r.raise_for_status()
-
 
 
 def get_pathway_col_list(cdf: pd.DataFrame) -> list:
@@ -86,7 +126,7 @@ def get_pathway_col_list(cdf: pd.DataFrame) -> list:
 
     :param df: Pathway specific DataFrame
     :type df: pd.DataFrame
-    :return: list of pathway col names 
+    :return: list of pathway col names
     :rtype: list
     """
     return list(set(list(cdf)) - set(components_non_pathway_cols))
--- cdr_verification/tests/test_postprocess.py
+++ cdr_verification/tests/test_postprocess.py
@@ -1,7 +1,38 @@
 import pytest
-from cdr_verification.common import test_google_doc_id, avail_pathways
-from cdr_verification.postprocess import (generate_combined_pathway_data_dict, equation_number_component_number, pathways_version_note_bool, pathway_componets_sheets_subset, pathway_id_sheets_subset, contributor_pathway_subset_bool,latest_pathway_version_match, pathway_uncertainty_range, unique_component_id, non_empty_component_name,non_empty_quantification_target, non_empty_description, valid_uncertainty_type, valid_responsibility, valid_uncertainty_min, valid_uncertainty_max, non_missing_pathways, non_empty_pathway_id)
-from cdr_verification.sheets_to_json import get_component_sheet, get_pathway_col_list, contributors_df
+
+from cdr_verification.common import avail_pathways, test_google_doc_id
+from cdr_verification.postprocess import (
+    contributor_pathway_subset_bool,
+    equation_number_component_number,
+    generate_combined_pathway_data_dict,
+    latest_pathway_version_match,
+    non_empty_component_name,
+    non_empty_description,
+    non_empty_equation,
+    non_empty_pathway_description,
+    non_empty_pathway_id,
+    non_empty_pathway_name,
+    non_empty_quantification_target,
+    non_empty_revision_note,
+    non_empty_VCL,
+    non_empty_version_number,
+    non_missing_pathways,
+    pathway_componets_sheets_subset,
+    pathway_id_sheets_subset,
+    pathway_uncertainty_range,
+    pathways_version_note_bool,
+    unique_component_id,
+    valid_responsibility,
+    valid_uncertainty_max,
+    valid_uncertainty_min,
+    valid_uncertainty_type,
+)
+from cdr_verification.sheets_to_json import (
+    contributors_df,
+    get_component_sheet,
+    get_pathway_col_list,
+)
+
 # The goal of these tests are to pull data from the test workbook
 # Which has examples of each error and test each postprocess step
 #
@@ -11,33 +42,36 @@ from cdr_verification.sheets_to_json import get_component_sheet, get_pathway_col
 # ------------- Test Pathways Sheets ---------------
 # --------------------------------------------------
 
-## Pathways Sheets
-# - [ x ]  (Static - Postprocess) There is a version note corresponding with the current version number
-# - [ x ]  (Static - Postprocess) All equation numbers correspond with numbered components (ie all numbers in equation are a subset of the numbers found in the number column)
+notification_flag = True
 
-notification_flag = True 
 
- 
 # --------------------------------------------------
 #               Fixtures
 # --------------------------------------------------
 
-@pytest.fixture(scope="module")
-def pathway_dict():
-    yield generate_combined_pathway_data_dict(google_doc_id=test_google_doc_id, avail_pathways=avail_pathways)
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope='module')
+def pathway_dict():
+    yield generate_combined_pathway_data_dict(
+        google_doc_id=test_google_doc_id, avail_pathways=avail_pathways
+    )
+
+
+@pytest.fixture(scope='module')
 def cdf():
     yield get_component_sheet(google_doc_id=test_google_doc_id)
 
-@pytest.fixture(scope="module")
+
+@pytest.fixture(scope='module')
 def cont_df():
     yield contributors_df(google_doc_id=test_google_doc_id)
 
-@pytest.fixture(scope="module")
+
+@pytest.fixture(scope='module')
 def pathway_col_list():
     cdf = get_component_sheet(google_doc_id=test_google_doc_id)
     yield get_pathway_col_list(cdf)
+
 
 # --------------------------------------------------
 #               Tests
@@ -47,80 +81,135 @@ def pathway_col_list():
 # ------ Cross-check (Pathway / Component) ---------
 # --------------------------------------------------
 
+
 def test_equation_number_component_number(pathway_dict):
-    df = equation_number_component_number(metadata_combined=pathway_dict, notification=notification_flag)
-    assert df['equation_number_bool'].all() == False
+    df = equation_number_component_number(
+        metadata_combined=pathway_dict, notification=notification_flag
+    )
+    assert df['equation_number_bool'].all() is False
+
 
 def test_pathways_version_note_bool(pathway_dict):
     df = pathways_version_note_bool(metadata_combined=pathway_dict, notification=notification_flag)
-    assert df['latest_version_note_exists'].all() == False
+    assert df['latest_version_note_exists'].all() is False
+
 
 def test_pathway_componets_sheets_subset(pathway_dict, cdf):
-    df = pathway_componets_sheets_subset(metadata_combined=pathway_dict, cdf=cdf, notification=notification_flag)
-    assert df['component_id_bool'].any() == False 
-    
-def test_pathway_id_sheets_subset(pathway_dict,cdf, pathway_col_list):
-    df = pathway_id_sheets_subset(metadata_combined=pathway_dict, cdf=cdf, pathway_col_list=pathway_col_list)
-    assert df['pathway_id_is_subset_component_id'].all() == False
+    df = pathway_componets_sheets_subset(
+        metadata_combined=pathway_dict, cdf=cdf, notification=notification_flag
+    )
+    assert df['component_id_bool'].any() is False
 
-def test_contributor_pathway_subset_bool(cdf,cont_df,pathway_col_list):
-    df = contributor_pathway_subset_bool(cdf=cdf, cont_df=cont_df, pathway_col_list = pathway_col_list)
-    assert df.empty == False
+
+def test_pathway_id_sheets_subset(pathway_dict, cdf, pathway_col_list):
+    df = pathway_id_sheets_subset(
+        metadata_combined=pathway_dict, cdf=cdf, pathway_col_list=pathway_col_list
+    )
+    assert df['pathway_id_is_subset_component_id'].all() is False
+
+
+def test_contributor_pathway_subset_bool(cdf, cont_df, pathway_col_list):
+    df = contributor_pathway_subset_bool(
+        cdf=cdf, cont_df=cont_df, pathway_col_list=pathway_col_list
+    )
+    assert df.empty is False
+
 
 def test_latest_pathway_version_match(pathway_dict, cont_df):
     df = latest_pathway_version_match(metadata_combined=pathway_dict, cont_df=cont_df)
-    assert df.empty == False
+    assert df.empty is False
 
-def test_pathway_uncertainty_range(pathway_dict,cdf):
+
+def test_pathway_uncertainty_range(pathway_dict, cdf):
     df = pathway_uncertainty_range(metadata_combined=pathway_dict, cdf=cdf)
-    assert df.empty == False
-    
+    assert df.empty is False
+
 
 # --------------------------------------------------
 # ---------------- Components Sheets ---------------
 # --------------------------------------------------
 
+
 def test_unique_component_id(cdf):
     df = unique_component_id(cdf=cdf)
-    assert df.empty == False
+    assert df.empty is False
+
 
 def test_non_empty_component_name(cdf):
     df = non_empty_component_name(cdf=cdf)
-    assert df.empty == False
+    assert df.empty is False
+
 
 def test_non_empty_quantification_target(cdf):
     df = non_empty_quantification_target(cdf=cdf)
-    assert df.empty == False
+    assert df.empty is False
+
 
 def test_non_empty_description(cdf):
     df = non_empty_description(cdf=cdf)
-    assert df.empty == False
+    assert df.empty is False
+
 
 def test_valid_uncertainty_type(cdf):
     df = valid_uncertainty_type(cdf=cdf)
-    assert df.empty == False
+    assert df.empty is False
+
 
 def test_valid_responsibility(cdf):
     df = valid_responsibility(cdf=cdf)
-    assert df.empty==False
+    assert df.empty is False
+
 
 def test_valid_uncertainty_min(cdf):
     df = valid_uncertainty_min(cdf=cdf)
-    assert df.empty==False
+    assert df.empty is False
+
 
 def test_valid_uncertainty_max(cdf):
     df = valid_uncertainty_max(cdf=cdf)
-    assert df.empty==False
+    assert df.empty is False
+
 
 def test_non_missing_pathways(cdf):
     df = non_missing_pathways(cdf=cdf)
-    assert df.empty==False
+    assert df.empty is False
 
 
 # --------------------------------------------------
 # ---------------- Pathways Sheets ---------------
 # --------------------------------------------------
 
+
 def test_non_empty_pathway_id(pathway_dict):
     df = non_empty_pathway_id(metadata_combined=pathway_dict)
-    assert df.empty==False
+    assert df.empty is False
+
+
+def test_non_empty_pathway_name(pathway_dict):
+    df = non_empty_pathway_name(metadata_combined=pathway_dict)
+    assert df.empty is False
+
+
+def test_non_empty_pathway_description(pathway_dict):
+    df = non_empty_pathway_description(metadata_combined=pathway_dict)
+    assert df.empty is False
+
+
+def test_non_empty_VCL(pathway_dict):
+    df = non_empty_VCL(metadata_combined=pathway_dict)
+    assert df.empty is False
+
+
+def test_non_empty_equation(pathway_dict):
+    df = non_empty_equation(metadata_combined=pathway_dict)
+    assert df.empty is False
+
+
+def test_non_empty_version_number(pathway_dict):
+    df = non_empty_version_number(metadata_combined=pathway_dict)
+    assert df.empty is False
+
+
+def test_non_empty_revision_note(pathway_dict):
+    df = non_empty_revision_note(metadata_combined=pathway_dict)
+    assert df.empty is False
