c17d4b248d13d50d5a125dd9a507fde08b9db461
Raphael Hagen <norlandrhagen@gmail.com>
2023-10-10 09:44:43 -0700

    force rerun checks with no real change

--- cdr_verification/postprocess.py
+++ cdr_verification/postprocess.py
@@ -35,8 +35,9 @@
 # ------------------ Imports -----------------------
 import re
 
-import numpy as np
 import pandas as pd  # type: ignore
+import numpy as np
+
 
 from .common import (
     avail_pathways,
