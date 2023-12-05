f59bd774f4422f9c855792b61d854a9dbc294874
katamartin <katasm@gmail.com>
2023-12-05 17:17:59 -0500

    Pull latest data and duplicate EW 1.2 flow diagram

--- components/flow-diagrams/EW/index.js
+++ components/flow-diagrams/EW/index.js
@@ -1,9 +1,11 @@
 import { default as v1dot0 } from './1.0'
 import { default as v1dot1 } from './1.1'
 import { default as v1dot2 } from './1.2'
+import { default as v1dot3 } from './1.3'
 
 export default {
   '1.0': v1dot0,
   1.1: v1dot1,
   1.2: v1dot2,
+  1.3: v1dot3,
 }
