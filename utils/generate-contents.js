5e10c3f5dffa13589f02f2ac512cfcfd2d8fe496
Kata Martin <katasm@gmail.com>
2026-04-22 12:16:42 -0400

    Reformat date

--- utils/generate-contents.js
+++ utils/generate-contents.js
@@ -4,17 +4,23 @@ const { pathways, pathwayContent } = require('./data.js')
 
 // Generate the contents list
 const fileContent = JSON.stringify(
-  pathways.map(({ pathway_id, pathway_name, pathway_description }) => ({
-    page: `research/cdr-verification/${pathway_id}`,
-    date: pathwayContent[pathway_id].metadata.revisions.sort(
+  pathways.map(({ pathway_id, pathway_name, pathway_description }) => {
+    const date = pathwayContent[pathway_id].metadata.revisions.sort(
       (a, b) => Number(b.version) - Number(a.version)
-    )[0].date,
-    metadata: {
-      type: 'tool',
-      title: pathway_name,
-      summary: pathway_description,
-    },
-  }))
+    )[0].date
+    const [mm, dd, yyyy] = date.split('-')
+    const reformattedDate = `${yyyy}-${mm}-${dd}`
+
+    return {
+      page: `research/cdr-verification/${pathway_id}`,
+      date: reformattedDate,
+      metadata: {
+        type: 'tool',
+        title: pathway_name,
+        summary: pathway_description,
+      },
+    }
+  })
 )
 
 // Write the contents.json file
